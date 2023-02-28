8a9a0271184ab3cf5807328c065e6b835ac74efa
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-02-28 20:20:35 +0800

    ...

--- src/db/user.rs
+++ src/db/user.rs
@@ -3,45 +3,13 @@ use crate::{
     models::user::{CreateUser, Role, Status, UpdateUser, User},
     utils,
 };
-use sqlx::{query_as, PgPool};
+use sqlx::PgPool;
 
 pub async fn post_new_user(pool: &PgPool, item: CreateUser) -> Result<User, Error> {
     item.valid().map_err(|e| Error::InvalidArgument(e.to_string()))?;
 
     // TODO: supporting enum convert between postgresql and rust in sqlx
-    let err = match query_as!(
-        User,
-        r#"INSERT INTO users
-          (status, role, phone, email, name, birthday) VALUES ($1, $2, $3, $4, $5, $6)
-		RETURNING
-		  id, status AS "status: Status", role AS "role: Role",
-		  phone, email, name, birthday, created_at, updated_at"#,
-        Status::OK as Status,
-        Role::Member as Role,
-        item.phone,
-        item.email,
-        item.name,
-        item.birthday,
-    )
-    .fetch_one(pool)
-    .await
-    {
-        Ok(v) => return Ok(v),
-        Err(e) => e,
-    };
-
-    if utils::db_error_code(&err) == Some("23505".into()) {
-        Err(Error::AlreadyExists)
-    } else {
-        Err(Error::DBError(err))
-    }
-}
-
-pub async fn post_update_user(pool: &PgPool, item: CreateUser) -> Result<User, Error> {
-    item.valid().map_err(|e| Error::InvalidArgument(e.to_string()))?;
-
-    // TODO: supporting enum convert between postgresql and rust in sqlx
-    let err = match query_as!(
+    let err = match sqlx::query_as!(
         User,
         r#"INSERT INTO users
           (status, role, phone, email, name, birthday) VALUES ($1, $2, $3, $4, $5, $6)
@@ -92,7 +60,8 @@ pub async fn update_user_details(
     )
     .fetch_one(pool)
     .await
-    .map_err(|_err| Error::NotFound("user not found".into()))?; // ignore database errors other than not found
+    .map_err(|_err| Error::NotFound("user not found".into()))?;
+    // ignore database errors other than NotFound
 
     if !user.update(update_user) {
         return Err(Error::NoChanges);
