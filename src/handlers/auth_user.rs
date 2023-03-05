79efddfaec14040ea24e21c470ba49e44265d7d8
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-05 22:18:41 +0800

    ...

--- src/handlers/auth_user.rs
+++ src/handlers/auth_user.rs
@@ -50,27 +50,27 @@ pub async fn update_user_details_v2b(
 // POST /user/update + BODY, update_user_details_b
 pub async fn update_user_details_v3(
     app_state: web::Data<AppState>,
-    jwt_payload: ReqData<JwtPayload>,
+    jwt: ReqData<JwtPayload>,
     item: web::Json<UpdateUser>,
 ) -> Result<HttpResponse, Error> {
-    db_user::update_user_details_b(&app_state.pool, jwt_payload.user_id, item.into_inner()).await?;
+    db_user::update_user_details_b(&app_state.pool, jwt.user_id, item.into_inner()).await?;
 
     Ok(HttpResponse::Ok().content_type(ContentType::json()).body(OK_JSON))
 }
 
 pub async fn user_details(
     app_state: web::Data<AppState>,
-    jwt_payload: ReqData<JwtPayload>,
+    jwt: ReqData<JwtPayload>,
 ) -> Result<HttpResponse, Error> {
-    let match_user = MatchUser { id: Some(jwt_payload.user_id), ..Default::default() };
+    let match_user = MatchUser { id: Some(jwt.user_id), ..Default::default() };
     db_user::find_user(&app_state.pool, match_user).await.map(|v| Ok(Data(v).into()))?
 }
 
 pub async fn frozen_user_status(
     app_state: web::Data<AppState>,
-    jwt_payload: ReqData<JwtPayload>,
+    jwt: ReqData<JwtPayload>,
 ) -> Result<HttpResponse, Error> {
-    let uus = UpdateUserStatus { id: jwt_payload.user_id, status: Status::Frozen };
+    let uus = UpdateUserStatus { id: jwt.user_id, status: Status::Frozen };
 
     db_user::update_user_status(&app_state.pool, uus).await.map(|v| Ok(Data(v).into()))?
     // TODO: disable token
@@ -78,10 +78,10 @@ pub async fn frozen_user_status(
 
 pub async fn user_change_password(
     app_state: web::Data<AppState>,
-    jwt_payload: ReqData<JwtPayload>,
+    jwt: ReqData<JwtPayload>,
     item: web::Json<ChangePassword>,
 ) -> Result<HttpResponse, Error> {
-    db_user::user_change_password(&app_state.pool, jwt_payload.user_id, item.into_inner())
+    db_user::user_change_password(&app_state.pool, jwt.user_id, item.into_inner())
         .await
         .map(|v| Ok(Data(v).into()))?
     // TODO: disable token
