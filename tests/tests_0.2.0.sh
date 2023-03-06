957033aa1eb6b207c60b8d7de2987843332f687d
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-06 18:33:01 +0800

    ...

--- tests/tests_0.2.0.sh
+++ tests/tests_0.2.0.sh
@@ -10,7 +10,7 @@ address=http://localhost:3000
 curl -i -X POST -H "content-type: application/json" "$address/api/open/user/login" \
   -d '{"email": "admin@users.noreply.github.com", "password": "12QWas!@"}'
 
-token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzgwMDUyMTksImV4cCI6MTY3ODAwNzAxOSwidXNlcklkIjoxLCJyb2xlIjoiYWRtaW4ifQ.QYv_JDycmL-Ob0zveRObjwjqE5KAQUON_95a9uk2F8k"
+token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzgwOTg3MjcsImV4cCI6MTY3ODEwMDUyNywidG9rZW5JZCI6ImM2ZGExNDU4LTUwMDYtNDM2Ny05MWE1LTZlZDA4NTQ0MWU1ZiIsInVzZXJJZCI6MSwicm9sZSI6ImFkbWluIn0.dl0fY8CCBHvY3BgBrlOGwFsElbdAjZ-xDo_ngYlpVYw"
 
 curl -i -X POST -H "content-type: application/json" -H "Authorization: $token" \
   "$address/api/auth/user/change_password" \
