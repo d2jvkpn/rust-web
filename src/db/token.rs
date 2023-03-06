273db1b7a03e65ab1cdb45e07d12df5b0f21ee49
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-07 02:35:16 +0800

    ...

--- src/db/token.rs
+++ src/db/token.rs
@@ -9,8 +9,8 @@ pub async fn save_token(pool: &PgPool, token: Token) -> Result<(), response::Err
 
     sqlx::query!(
         r#"INSERT INTO tokens
-              (token_id, user_id, iat, exp, ip, platform, device, status)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, true)"#,
+          (token_id, user_id, iat, exp, ip, platform, device, status)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, true)"#,
         token.token_id,
         token.user_id,
         token.iat,
