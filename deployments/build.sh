f394a71cbc3a227b58254e491fc4a99b2e189df9
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-07 16:39:05 +0800

    ...

--- deployments/build.sh
+++ deployments/build.sh
@@ -7,7 +7,7 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 name=registry.cn-shanghai.aliyuncs.com/d2jvkpn/rust-web
 tag=dev
 
-docker build --no-cache -f ${_path}/build.df --tag $name:$tag ./
+docker build --no-cache -f ${_path}/Dockerfile --tag $name:$tag ./
 
 docker image prune --force --filter label=stage=rust-web_builder &> /dev/null
 
