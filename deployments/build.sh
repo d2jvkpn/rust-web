09af15888902c263d178ee650607a913b8fcf6bf
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-13 11:01:01 +0800

    ...

--- deployments/build.sh
+++ deployments/build.sh
@@ -7,6 +7,15 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 name=registry.cn-shanghai.aliyuncs.com/d2jvkpn/rust-web
 tag=dev
 
+bash deployments/git-build-info.sh > .env_git-build-info
+
+for base in $(awk '/^FROM/{print $2}' ${_path}/Dockerfile); do
+    docker pull --quiet $base
+    bn=$(echo $base | awk -F ":" '{print $1}')
+    if [[ -z "$bn" ]]; then continue; fi
+    docker images --filter "dangling=true" --quiet "$bn" | xargs -i docker rmi {}
+done &> /dev/null
+
 docker build --no-cache -f ${_path}/Dockerfile --tag $name:$tag ./
 docker image prune --force --filter label=stage=rust-web_builder &> /dev/null
 
