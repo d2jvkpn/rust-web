528f818fcc5af397533b319f7009fd9962c3c5d7
d2jvkpn <d2jvkpn@users.noreply.github.com>
2023-03-13 10:48:11 +0800

    ...

--- src/utils/git_build_info.rs
+++ src/utils/git_build_info.rs
@@ -14,16 +14,12 @@ pub struct GitBuildInfo {
 }
 
 //
-pub static GIT_BUILD_INFO: Lazy<GitBuildInfo> = Lazy::new(|| {
-    let data = GitBuildInfo {
-        build_time: option_env!("BUILD_Time").map(|v| v.to_string()),
-        git_branch: option_env!("GIT_Branch").map(|v| v.to_string()),
-        git_commit: option_env!("GIT_Commit").map(|v| v.to_string()),
-        git_time: option_env!("GIT_Time").map(|v| v.to_string()),
-        git_tree_state: option_env!("GIT_TreeState").map(|v| v.to_string()),
-    };
-
-    data
+pub static GIT_BUILD_INFO: Lazy<GitBuildInfo> = Lazy::new(|| GitBuildInfo {
+    build_time: option_env!("BUILD_Time").map(|v| v.to_string()),
+    git_branch: option_env!("GIT_Branch").map(|v| v.to_string()),
+    git_commit: option_env!("GIT_Commit").map(|v| v.to_string()),
+    git_time: option_env!("GIT_Time").map(|v| v.to_string()),
+    git_tree_state: option_env!("GIT_TreeState").map(|v| v.to_string()),
 });
 
 /* GIT_BUILD_INFO.lock().unwrap_or(GitBuildInfo::default()) */
@@ -33,15 +29,15 @@ static OC_GBI: OnceCell<GitBuildInfo> = OnceCell::new();
 
 impl GitBuildInfo {
     pub fn set() -> Result<(), &'static str> {
-        let build_time = option_env!("BUILD_Time").map(|v| v.to_string());
-        let git_branch = option_env!("GIT_Branch").map(|v| v.to_string());
-        let git_commit = option_env!("GIT_Commit").map(|v| v.to_string());
-        let git_time = option_env!("GIT_Time").map(|v| v.to_string());
-        let git_tree_state = option_env!("GIT_TreeState").map(|v| v.to_string());
+        let data = Self {
+            build_time: option_env!("BUILD_Time").map(|v| v.to_string()),
+            git_branch: option_env!("GIT_Branch").map(|v| v.to_string()),
+            git_commit: option_env!("GIT_Commit").map(|v| v.to_string()),
+            git_time: option_env!("GIT_Time").map(|v| v.to_string()),
+            git_tree_state: option_env!("GIT_TreeState").map(|v| v.to_string()),
+        };
 
-        OC_GBI
-            .set(Self { build_time, git_branch, git_commit, git_time, git_tree_state })
-            .map_err(|_| "can't set GitBuildInfo")
+        OC_GBI.set(data).map_err(|_| "can't set GitBuildInfo")
     }
 
     pub fn get() -> Option<&'static GitBuildInfo> {
